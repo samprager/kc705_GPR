8a3935a46ebc0ec40e23d92d5f3b28adceaec9cb
samprager <sprager@usc.edu>
2016-06-22 17:44:49 -0700

    unclear changes

--- Design/Firmware/sim/packetizer/sim_tb_packetizer.v
+++ Design/Firmware/sim/packetizer/sim_tb_packetizer.v
@@ -3,20 +3,20 @@
 module sim_tb_packetizer;
 
    localparam AXI_TCLK_PERIOD          = 8000;         // 125 MHz
-  localparam RESET_PERIOD = 320000; //in pSec  
+  localparam RESET_PERIOD = 320000; //in pSec
 
     reg axi_tresetn_i;
     reg axi_tclk_i;
-    
+
    wire                   axi_tclk;
    wire                   axi_tresetn;
-  
+
      // data from ADC Data fifo
   reg       [7:0]                    adc_axis_tdata_reg;
   reg                                adc_axis_tvalid_reg;
   reg                                adc_axis_tlast_reg;
   reg                                adc_axis_tuser_reg;
-  
+
          // data from ADC Data fifo
 wire       [7:0]                    adc_axis_tdata;
 wire                                adc_axis_tvalid;
@@ -24,19 +24,19 @@ wire                                adc_axis_tlast;
 wire                                adc_axis_tuser;
 
  wire                               adc_axis_tready;
-  
- 
+
+
  wire  [7:0]       tdata;
  wire              tvalid;
  wire              tlast;
  wire              tready;
- 
+
  wire       [7:0]                    rx_axis_tdata;
  wire                                rx_axis_tvalid;
  wire                                rx_axis_tlast;
  wire                                rx_axis_tuser;
  wire                               rx_axis_tready;
- 
+
  reg       [7:0]                    rx_axis_tdata_reg;
  reg                                rx_axis_tvalid_reg;
  reg                                rx_axis_tlast_reg;
@@ -50,12 +50,12 @@ wire                                adc_axis_tuser;
  wire                               tx_axis_tvalid;
  wire                               tx_axis_tlast;
  wire                                tx_axis_tready;
- 
+
  reg                                tx_axis_tready_reg;
- 
+
  reg                tready_reg;
  reg                adc_axis_tvalid_select;
- 
+
  wire     frame_error;
  wire activity_flash;
  //**************************************************************************//
@@ -71,20 +71,20 @@ wire                                adc_axis_tuser;
   // Clock Generation
   //**************************************************************************//
 
-  initial 
+  initial
     begin
         axi_tclk_i = 1'b0;
     end
-  always 
+  always
     begin
         axi_tclk_i = #(AXI_TCLK_PERIOD/2.0) ~axi_tclk_i;
     end
-    
-    
+
+
    assign axi_tresetn = axi_tresetn_i;
    assign axi_tclk = axi_tclk_i;
-   
-        
+
+
  initial begin
       tx_axis_tready_reg = 1'b1; // initial value
       @(posedge axi_tresetn_i); // wait for reset
@@ -102,21 +102,21 @@ wire                                adc_axis_tuser;
       repeat(1) @(posedge axi_tclk_i);
       tx_axis_tready_reg = 1'b1;
       repeat(2) @(posedge axi_tclk_i);
-      tx_axis_tready_reg = 1'b0;  
+      tx_axis_tready_reg = 1'b0;
       repeat(1) @(posedge axi_tclk_i);
       tx_axis_tready_reg = 1'b1;
       repeat(1) @(posedge axi_tclk_i);
-      tx_axis_tready_reg = 1'b0; 
+      tx_axis_tready_reg = 1'b0;
       repeat(256) @(posedge axi_tclk_i);
-      tx_axis_tready_reg = 1'b1;        
+      tx_axis_tready_reg = 1'b1;
       repeat(2048) @(posedge axi_tclk_i);
-      tx_axis_tready_reg = 1'b0;        
+      tx_axis_tready_reg = 1'b0;
       repeat(2048) @(posedge axi_tclk_i);
-      tx_axis_tready_reg = 1'b1;        
+      tx_axis_tready_reg = 1'b1;
       repeat(2048) @(posedge axi_tclk_i);
       $finish;
-    end     
- 
+    end
+
  always @(posedge  axi_tclk_i) begin
     if (~axi_tresetn_i) begin
         adc_axis_tdata_reg <= 'b1;
@@ -129,11 +129,11 @@ wire                                adc_axis_tuser;
             adc_axis_tdata_reg <= adc_axis_tdata_reg + 1'b1;
         if (&adc_axis_tdata_reg[5:0])
             adc_axis_tlast_reg <= 1'b1;
-        else    
-            adc_axis_tlast_reg <= 1'b0;     
+        else
+            adc_axis_tlast_reg <= 1'b0;
     end
 end
-        
+
  initial begin
      adc_axis_tvalid_select = 1'b0; // initial value
      @(posedge axi_tresetn_i); // wait for reset
@@ -151,25 +151,25 @@ end
      repeat(1) @(posedge axi_tclk_i);
      adc_axis_tvalid_select = 1'b1;
      repeat(2) @(posedge axi_tclk_i);
-     adc_axis_tvalid_select = 1'b0;  
+     adc_axis_tvalid_select = 1'b0;
      repeat(1) @(posedge axi_tclk_i);
      adc_axis_tvalid_select = 1'b1;
      repeat(1) @(posedge axi_tclk_i);
-     adc_axis_tvalid_select = 1'b0; 
+     adc_axis_tvalid_select = 1'b0;
      repeat(20) @(posedge axi_tclk_i);
-     adc_axis_tvalid_select = 1'b1;     
+     adc_axis_tvalid_select = 1'b1;
      repeat(1000) @(posedge axi_tclk_i);
-     adc_axis_tvalid_select = 1'b0; 
+     adc_axis_tvalid_select = 1'b0;
      repeat(2000) @(posedge axi_tclk_i);
-     adc_axis_tvalid_select = 1'b1;     
-   end         
-   
+     adc_axis_tvalid_select = 1'b1;
+   end
+
 initial begin
     rx_axis_tdata_reg = 7'b0;
     rx_axis_tvalid_reg = 1'b0;
     rx_axis_tlast_reg = 1'b0;
     rx_axis_tuser_reg = 1'b0;
-end   
+end
 
  kc705_ethernet_rgmii_basic_pat_gen #(
  //   parameter                            DEST_ADDR       = 48'hda0102030405,
@@ -208,8 +208,8 @@ end
 
 // data from ADC Data fifo
     .enable_adc_pkt         (1'b1),
-    .adc_axis_tdata               (adc_axis_tdata), 
-    .adc_axis_tvalid              (adc_axis_tvalid), 
+    .adc_axis_tdata               (adc_axis_tdata),
+    .adc_axis_tvalid              (adc_axis_tvalid),
     .adc_axis_tlast              (adc_axis_tlast),
     .adc_axis_tuser                 (adc_axis_tuser),
     .adc_axis_tready              (adc_axis_tready),
@@ -217,8 +217,8 @@ end
 
     .frame_error (frame_error),
     .activity_flash (activity_flash)
-); 
- 
+);
+
 //kc705_ethernet_rgmii_axi_packetizer u_packetizer_top
 //(
 //       .axi_tclk (axi_tclk),
@@ -228,8 +228,8 @@ end
 //        .speed  (2'b10),
 
 //    // data from ADC Data fifo
-//        .adc_axis_tdata               (adc_axis_tdata), 
-//        .adc_axis_tvalid              (adc_axis_tvalid), 
+//        .adc_axis_tdata               (adc_axis_tdata),
+//        .adc_axis_tvalid              (adc_axis_tvalid),
 //        .adc_axis_tlast              (adc_axis_tlast),
 //        .adc_axis_tuser          (adc_axis_tuser),
 //        .adc_axis_tready              (adc_axis_tready),
@@ -255,6 +255,6 @@ assign rx_axis_tuser =  adc_axis_tuser_reg;
 //assign rx_axis_tvalid =  rx_axis_tdata_reg;
 //assign rx_axis_tlast =  rx_axis_tlast_reg;
 //assign rx_axis_tuser =  rx_axis_tuser_reg;
-  
-  
+
+
 endmodule
