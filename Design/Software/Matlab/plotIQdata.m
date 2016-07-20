7d00fb517eaf93f1aa169b00d3967b45dbc66720
samprager <sprager@usc.edu>
2016-07-19 21:50:31 -0700

    added support for counterless packets

--- Design/Software/Matlab/plotIQdata.m
+++ Design/Software/Matlab/plotIQdata.m
@@ -2,10 +2,12 @@
 %filename = '../outputs/adc_chirp.bin'; bitformat = 'uint32';
 %filename = '../outputs/chirp_shifted.bin'; bitformat = 'uint32';
 %filename = '../C/Listener/outdata.bin'; bitformat = 'uint32';
-filename = '/Users/sam/outputs/adc_chirp.bin'; bitformat = 'uint32';
+filename = '/Users/sam/outputs/en4_dataout.bin'; bitformat = 'uint32';
 byteoffset = 0;
 packetsize = 496;
 headersize = 16;
+
+has_counter = 0;
 if (strcmp(bitformat,'uint8')||strcmp(bitformat,'int8'))
     bytesperword = 1;
 elseif (strcmp(bitformat,'uint16')||strcmp(bitformat,'int16'))
@@ -21,38 +23,18 @@ size(A)
 numbytes = (packetsize-headersize)/bytesperword;
 format long;
 
-% find beginning of first 512 bit sub-packet
-subp_found = 0; 
-ind = 1;
 
-% First case: counter is upper 32 bits of each 64 bit word
-if A(1) == A(2*8-1)+7
-    subp_found = 1;
+if (has_counter)
+    % find beginning of first 512 bit sub-packet
+    subp_found = 0; 
     ind = 1;
-else
-    subp_found = 0; ind = 3;
-    while (subp_found ==0 && ind <= 64/bytesperword)
-        temp = A(ind);
-        if ((A(ind) ~= A(ind-2)-1)&&(A(ind+2) == A(ind)-1))
-            subp_found = 1;
-        else
-            ind = ind+2;
-        end
-    end
-end
-
-% counter offset supports counter in either lower or upper 32b of 64b word 
-if (subp_found == 1)
     counter_offset = 0;
-% Second case: counter is lower 32 bits of each 64 bit word    
-else 
-    counter_offset = 1;
-    if A(2) == A(2*8)+7
+    % First case: counter is upper 32 bits of each 64 bit word
+    if A(1) == A(2*8-1)+7
         subp_found = 1;
-        ind = 2;
-    else 
-        subp_found = 0;
-        ind = 4;
+        ind = 1;
+    else
+        subp_found = 0; ind = 3;
         while (subp_found ==0 && ind <= 64/bytesperword)
             temp = A(ind);
             if ((A(ind) ~= A(ind-2)-1)&&(A(ind+2) == A(ind)-1))
@@ -62,6 +44,34 @@ else
             end
         end
     end
+
+    % counter offset supports counter in either lower or upper 32b of 64b word 
+    if (subp_found == 1)
+        counter_offset = 0;
+    % Second case: counter is lower 32 bits of each 64 bit word    
+    else 
+        counter_offset = 1;
+        if A(2) == A(2*8)+7
+            subp_found = 1;
+            ind = 2;
+        else 
+            subp_found = 0;
+            ind = 4;
+            while (subp_found ==0 && ind <= 64/bytesperword)
+                temp = A(ind);
+                if ((A(ind) ~= A(ind-2)-1)&&(A(ind+2) == A(ind)-1))
+                    subp_found = 1;
+                else
+                    ind = ind+2;
+                end
+            end
+        end
+    end
+else
+% Manually find beginning of first 512 bit sub-packet
+    subp_found = 0; 
+    ind = 9;
+    counter_offset = 0;
 end
 
 % get rid of partial sub-packets at beginning
@@ -113,31 +123,27 @@ data_iq = dec2hex(data);
 I = double(typecast(uint16(hex2dec(data_iq(:,1:4))),'int16')); 
 Q = double(typecast(uint16(hex2dec(data_iq(:,5:end))),'int16'));
 
+data_iq2 = dec2hex(counter);
+I2 = double(typecast(uint16(hex2dec(data_iq2(:,1:4))),'int16')); 
+Q2 = double(typecast(uint16(hex2dec(data_iq2(:,5:end))),'int16'));
+
 % determine size and location of skips/drops
-num_drops = counter(end)-counter(1)-numel(counter)+1;
-counter_jumps = [];
-for i=2:numel(counter)
-    if (counter(i) ~= counter(i-1)+1)
-        counter_jumps = [counter_jumps, i];
+if (has_counter)
+    num_drops = counter(end)-counter(1)-numel(counter)+1;
+    counter_jumps = [];
+    for i=2:numel(counter)
+        if (counter(i) ~= counter(i-1)+1)
+            counter_jumps = [counter_jumps, i];
+        end
     end
+    counter_jumps = [counter_jumps, numel(counter)];
+else     
+    counter_jumps = [1, numel(counter)];
 end
-counter_jumps = [counter_jumps, numel(counter)];
 
 % Plot decoded counter
-%chirpmin = 1; chirpmax = 5000; 
-chirpmin = counter_jumps(1); chirpmax = counter_jumps(2);
-
-figure; 
-subplot(3,1,1); hold on; 
-plot(counter); axis tight; title('Decoded 32 bit Counter');
-y1=get(gca,'ylim'); x1 = [chirpmin,chirpmax];
-line([x1;x1],[y1',y1'],'Color','r');hold off;
-% Plot decoded data
-subplot (3,1,2); plot(I(chirpmin:chirpmax)); 
-axis tight; title('Decoded I'); 
-
-subplot(3,1,3); plot(Q(chirpmin:chirpmax)); 
-axis tight; title('Decoded Q');
+chirpmin = 1; chirpmax = 5000; 
+%chirpmin = counter_jumps(1); chirpmax = counter_jumps(2);
 
 % Plot a PSD with 90% Occupied BW of IQ data
 Fs = 245.76e6;
@@ -152,20 +158,56 @@ title(['Q Channel: ',get(get(gca,'title'),'string')]);
 %figure; obw(x,Fs); title(['x = I+jQ: ',get(get(gca,'title'),'string')]);
 %figure; plot(20*log10(abs(fftshift(fft(x))))); title('fft of I+i*Q');
 
-% Plot complete data set with locations of counter jumps 
-figure; 
-subplot(3,1,1); hold on;
-plot(counter); title('Counter jump/skip locations');
-scatter(counter_jumps,counter(counter_jumps)); axis tight; hold off;
+if (has_counter)
+    % Plot complete data set with locations of counter jumps 
+    figure; 
+    subplot(3,1,1); hold on; 
+    plot(counter); axis tight; title('Decoded 32 bit Counter');
+    y1=get(gca,'ylim'); x1 = [chirpmin,chirpmax];
+    line([x1;x1],[y1',y1'],'Color','r');hold off;
+    % Plot decoded data
+    subplot (3,1,2); plot(I(chirpmin:chirpmax)); 
+    axis tight; title('Decoded I'); 
 
-subplot (3,1,2); hold on; 
-plot(I); title('Decoded I');
-scatter(counter_jumps,I(counter_jumps)); axis tight; hold off;
+    subplot(3,1,3); plot(Q(chirpmin:chirpmax)); 
+    axis tight; title('Decoded Q');
 
-subplot(3,1,3); hold on;
-plot(Q); title('Decoded Q');
-scatter(counter_jumps,Q(counter_jumps)); axis tight; hold off;
+    figure; 
+    subplot(3,1,1); hold on;
+    plot(counter); title('Counter jump/skip locations');
+    scatter(counter_jumps,counter(counter_jumps)); axis tight; hold off;
 
+    subplot (3,1,2); hold on; 
+    plot(I); title('Decoded I');
+    scatter(counter_jumps,I(counter_jumps)); axis tight; hold off;
+
+    subplot(3,1,3); hold on;
+    plot(Q); title('Decoded Q');
+    scatter(counter_jumps,Q(counter_jumps)); axis tight; hold off;
+else    
+    figure; 
+    % Plot decoded data
+    subplot (4,1,1); plot(I(chirpmin:chirpmax)); 
+    axis tight; title('Decoded I'); 
+
+    subplot(4,1,2); plot(I2(chirpmin:chirpmax)); 
+    axis tight; title('Decoded I2');
+    
+    subplot (4,1,3); plot(Q(chirpmin:chirpmax)); 
+    axis tight; title('Decoded Q'); 
+
+    subplot(4,1,4); plot(Q2(chirpmin:chirpmax)); 
+    axis tight; title('Decoded Q2');
+
+    figure; 
+    subplot(2,1,1); hold on;
+    plot(I);plot(I2,'r');
+    title('I and I2 Channels');legend('I','I2');axis tight; hold off;
+    subplot(2,1,2); hold on;
+    plot(Q);plot(Q2,'r');
+    title('Q and Q2 Channels');legend('Q','Q2');axis tight; hold off;
+
+end
 figure; hold on;
 plot(I);plot(Q,'r');
 title('I and Q Channels');legend('I','Q');axis tight; hold off;
