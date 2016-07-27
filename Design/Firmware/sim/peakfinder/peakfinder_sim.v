8cfc3ac7352e70cc80c1d650f3591f091d808cf5
samprager <sprager@usc.edu>
2016-07-26 20:13:00 -0700

    peakfinder complete

--- Design/Firmware/sim/peakfinder/peakfinder_sim.v
+++ Design/Firmware/sim/peakfinder/peakfinder_sim.v
@@ -38,6 +38,8 @@ module peakfinder_sim(
         reg fmc_tclk_i;
 
         reg [7:0] counter;
+        reg [7:0] index;
+        reg [7:0] test_data;
 
            wire                   axi_tclk;
         wire                   axi_tresetn;
@@ -68,20 +70,31 @@ module peakfinder_sim(
         wire        sq_mag_i_axis_tvalid;
         wire        sq_mag_i_axis_tlast;
         wire [31:0] sq_mag_i_axis_tuser;
+        wire [31:0] sq_mag_i_index;
         wire [63:0] sq_mag_q_axis_tdata;
         wire        sq_mag_q_axis_tvalid;
         wire        sq_mag_q_axis_tlast;
         wire [31:0] sq_mag_q_axis_tuser;
+        wire [31:0] sq_mag_q_index;
         wire       sq_mag_i_axis_tdata_overflow;
         wire       sq_mag_q_axis_tdata_overflow;
 
         wire [31:0] peak_index_i;
         wire [63:0] peak_tdata_i;
         wire peak_tvalid_i;
+         wire peak_tlast_i;
+        wire [31:0] peak_tuser_i;
+        wire [31:0]num_peaks_i;
 
         wire [31:0] peak_index_q;
         wire [63:0] peak_tdata_q;
         wire peak_tvalid_q;
+        wire peak_tlast_q;
+        wire [31:0] peak_tuser_q;
+        wire [31:0]num_peaks_q;
+        
+        reg [31:0] peak_result_i;
+        reg [31:0] peak_result_q;
 
      initial begin
           axi_tresetn_i = 1'b0;
@@ -153,12 +166,22 @@ module peakfinder_sim(
                   m_fft_q_axis_tvalid_r<= 1'b0;
                   m_fft_i_axis_tlast_r <= 1'b0;
                   m_fft_q_axis_tlast_r <= 1'b0;
+                  index <= 'b0;
+                  test_data <= 'b0;
+                  peak_result_i <= 'b0;
+                  peak_result_q <= 'b0;
               end else begin
                   counter <= counter + 1'b1;
-                  if (counter <= 8'hfa) begin
+                  index <= counter;
+                  if(counter <= 8'hcf)
+                    test_data <= counter;
+                  else
+                    test_data <= test_data - 1'b1;
+                    
+                  if (counter <= 8'hf0) begin
                     m_fft_i_axis_tvalid_r <= 1'b1;
                     m_fft_q_axis_tvalid_r<= 1'b1;
-                    if (counter == 8'hfa) begin
+                    if (counter == 8'hf0) begin
                       m_fft_i_axis_tlast_r <= 1'b1;
                       m_fft_q_axis_tlast_r <= 1'b1;
                     end else begin
@@ -171,11 +194,20 @@ module peakfinder_sim(
                     m_fft_i_axis_tlast_r <= 1'b0;
                     m_fft_q_axis_tlast_r <= 1'b0;
                   end
+                  
+                  if (peak_tlast_i & peak_tvalid_i)
+                    peak_result_i <= peak_index_i;
+                    
+                  if (peak_tlast_q & peak_tvalid_q)
+                    peak_result_q <= peak_index_q;
+                    
               end
+              
+              
             end
 
-assign m_fft_i_axis_tdata = {28'b0,counter[7:4],28'b0,counter[3:0]};
-assign m_fft_q_axis_tdata = {28'b0,counter[3:0],24'b0,counter};
+assign m_fft_i_axis_tdata = {28'b0,test_data[7:4],28'b0,test_data[3:0]};
+assign m_fft_q_axis_tdata = {28'b0,counter[7:4],28'b0,counter[3:0]};
 
  assign m_fft_i_axis_tready = 1'b1;
  assign m_fft_q_axis_tready = 1'b1;
@@ -184,6 +216,8 @@ assign m_fft_q_axis_tdata = {28'b0,counter[3:0],24'b0,counter};
 assign m_fft_q_axis_tvalid = m_fft_q_axis_tvalid_r;
 assign m_fft_i_axis_tlast = m_fft_i_axis_tlast_r;
 assign m_fft_q_axis_tlast = m_fft_q_axis_tlast_r;
+
+
 
     sq_mag_estimate#(
         .DATA_LEN(32),
@@ -198,11 +232,13 @@ assign m_fft_q_axis_tlast = m_fft_q_axis_tlast_r;
         .dataQ(m_fft_i_axis_tdata[63:32]),
         .dataQ_tvalid(m_fft_i_axis_tvalid),
         .dataQ_tlast(m_fft_i_axis_tlast),
+        .data_index({24'b0,index}),
         .data_tuser({24'b0,counter}),
         .dataMagSq(sq_mag_i_axis_tdata),
         .dataMag_tvalid(sq_mag_i_axis_tvalid),
         .dataMag_tlast(sq_mag_i_axis_tlast),
         .dataMag_tuser(sq_mag_i_axis_tuser),
+        .dataMag_index(sq_mag_i_index),
         .overflow(sq_mag_i_axis_tdata_overflow)
     );
 
@@ -219,11 +255,13 @@ assign m_fft_q_axis_tlast = m_fft_q_axis_tlast_r;
        .dataQ(m_fft_q_axis_tdata[63:32]),
        .dataQ_tvalid(m_fft_q_axis_tvalid),
        .dataQ_tlast(m_fft_q_axis_tlast),
+       .data_index({24'b0,index}),
        .data_tuser({24'b0,counter}),
        .dataMagSq(sq_mag_q_axis_tdata),
        .dataMag_tvalid(sq_mag_q_axis_tvalid),
        .dataMag_tlast(sq_mag_q_axis_tlast),
        .dataMag_tuser(sq_mag_q_axis_tuser),
+       .dataMag_index(sq_mag_q_index),
        .overflow(sq_mag_q_axis_tdata_overflow)
     );
 
@@ -235,11 +273,15 @@ assign m_fft_q_axis_tlast = m_fft_q_axis_tlast_r;
       .tdata(sq_mag_i_axis_tdata),
       .tvalid(sq_mag_i_axis_tvalid),
       .tlast(sq_mag_i_axis_tlast),
-      .index(sq_mag_i_axis_tuser),
+      .tuser(sq_mag_i_axis_tuser),
+      .index(sq_mag_i_index),
       .threshold({64'hff}),
       .peak_index(peak_index_i),
       .peak_tdata(peak_tdata_i),
-      .peak_tvalid(peak_tvalid_i)
+      .peak_tvalid(peak_tvalid_i),
+      .peak_tlast(peak_tlast_i),
+      .peak_tuser(peak_tuser_i),
+      .num_peaks(num_peaks_i)
     );
     peak_finder #(
       .DATA_LEN(64)
@@ -249,11 +291,15 @@ assign m_fft_q_axis_tlast = m_fft_q_axis_tlast_r;
       .tdata(sq_mag_q_axis_tdata),
       .tvalid(sq_mag_q_axis_tvalid),
       .tlast(sq_mag_q_axis_tlast),
-      .index(sq_mag_q_axis_tuser),
+      .tuser(sq_mag_q_axis_tuser),
+      .index(sq_mag_q_index),
       .threshold({64'hff}),
       .peak_index(peak_index_q),
       .peak_tdata(peak_tdata_q),
-      .peak_tvalid(peak_tvalid_q)
+      .peak_tvalid(peak_tvalid_q),
+      .peak_tlast(peak_tlast_q),
+      .peak_tuser(peak_tuser_q),
+      .num_peaks(num_peaks_q)
     );
 
 endmodule
--- Design/Firmware/source/custom/sq_mag_estimate.v
+++ Design/Firmware/source/custom/sq_mag_estimate.v
@@ -25,7 +25,8 @@ module sq_mag_estimate #(
     parameter DIV_OR_OVERFLOW = 0,      // (1): Divide output by 2, (0): use overflow bit
     parameter REGISTER_OUTPUT = 1,
     parameter MULT_LATENCY = 6,
-    parameter TUSER_LEN = 32
+    parameter TUSER_LEN = 32,
+    parameter INDEX_LEN = 32
     )(
     input clk,
     input [DATA_LEN-1:0] dataI,
@@ -35,10 +36,12 @@ module sq_mag_estimate #(
     input dataQ_tvalid,
     input dataQ_tlast,
     input [TUSER_LEN-1:0] data_tuser,
+    input [INDEX_LEN-1:0] data_index,
     output[2*DATA_LEN-1:0] dataMagSq,
     output dataMag_tvalid,
     output dataMag_tlast,
     output [TUSER_LEN-1:0] dataMag_tuser,
+    output [INDEX_LEN-1:0] dataMag_index,
     output overflow
     );
 
@@ -51,6 +54,7 @@ module sq_mag_estimate #(
     reg[MULT_LATENCY+REGISTER_OUTPUT-1:0] q_tvalid_shift;
     reg [MULT_LATENCY+REGISTER_OUTPUT-1:0] q_tlast_shift;
     reg [TUSER_LEN-1:0] tuser_shift [MULT_LATENCY+REGISTER_OUTPUT-1:0];
+    reg [INDEX_LEN-1:0] index_shift [MULT_LATENCY+REGISTER_OUTPUT-1:0];
 
     integer i;
 
@@ -73,18 +77,21 @@ module sq_mag_estimate #(
     q_tvalid_shift[0] <= dataQ_tvalid;
     q_tlast_shift[0] <= dataQ_tlast;
     tuser_shift[0] <= data_tuser;
+    index_shift[0] <= data_index;
     for(i=1;i<MULT_LATENCY+REGISTER_OUTPUT;i=i+1) begin
       i_tvalid_shift[i] <= i_tvalid_shift[i-1];
       i_tlast_shift[i] <= i_tlast_shift[i-1];
       q_tvalid_shift[i] <= q_tvalid_shift[i-1];
       q_tlast_shift[i] <= q_tlast_shift[i-1];
       tuser_shift[i] <= tuser_shift[i-1];
+      index_shift[i] <= index_shift[i-1];
     end
   end
 
   assign dataMag_tvalid = i_tvalid_shift[MULT_LATENCY+REGISTER_OUTPUT-1] & q_tvalid_shift[MULT_LATENCY+REGISTER_OUTPUT-1];
   assign dataMag_tlast = i_tlast_shift[MULT_LATENCY+REGISTER_OUTPUT-1] & q_tlast_shift[MULT_LATENCY+REGISTER_OUTPUT-1];
   assign dataMag_tuser = tuser_shift[MULT_LATENCY+REGISTER_OUTPUT-1];
+  assign dataMag_index = index_shift[MULT_LATENCY+REGISTER_OUTPUT-1];
 
 generate if(REGISTER_OUTPUT == 1) begin
   always @(posedge clk) begin
