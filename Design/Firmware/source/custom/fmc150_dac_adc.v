7cff268f83641fc622596a976d1df2e41f8e66d9
samprager <prager.sam@gmail.com>
2016-08-25 11:36:37 -0700

    changed waveform delay. seemingly no effect...

--- Design/Firmware/source/custom/fmc150_dac_adc.v
+++ Design/Firmware/source/custom/fmc150_dac_adc.v
@@ -179,7 +179,7 @@ module fmc150_dac_adc #
 
   localparam DDS_LATENCY = 2;
   localparam DDS_CHIRP_DELAY = 3;
-  localparam DDS_WFRM_DELAY = 19;
+  localparam DDS_WFRM_DELAY = 50;//33;//19;
   localparam FCUTOFF_IND = FFT_LEN/2;
 
   wire rd_fifo_clk;
